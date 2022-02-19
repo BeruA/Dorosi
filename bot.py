05473b6b189e3c58125cd22f3216c70afca01724
BeruA <49359687+BeruA@users.noreply.github.com>
2022-02-19 14:58:12 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -148,4 +148,4 @@ async def on_message(message):
 
 
 
-client.run("ODkzMTcwMTQyODI4MTY3MjA4.YVXjhA.AWhbXDzZAoH6p66uEIa3dqxv-Bs")
+client.run("ODkzMTcwMTQyODI4MTY3MjA4.YVXjhA.8Yso2FjPiYx6BDoxxLv6t5_ZuDc")
