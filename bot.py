b9ddf216e881a4a6f35a14f12d82cf7da8b715b9
BeruA <seven6974night@gmail.com>
2022-03-29 16:51:33 +0900

    ㅍ

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ async def on_ready():
     await client.change_presence(status=discord.Status.idle, activity=discord.Game('상태메시지'))
     await bt(['마감', '숙면', '식사', '하는 중 하는 중 하는 중 하는 중', '게임', '화공', '도발', '섹스'])
     def job():
-        channel = client.get_channels(810490718979489845)
+        channel = client.get_channels('810490718979489845')
         channel.send('폰 불출 시간!!!')
         
     await schedule.every().monday.at("17:30").do(job)
